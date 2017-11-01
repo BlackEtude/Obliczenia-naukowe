6eaa18306234707784a97d8c3e3dedf05b5fe283
BlackEtude <229726@student.pwr.edu.pl>
2017-11-01 15:52:57 +0100

    ex4 update

--- Lista2/zad4/zad4.jl
+++ Lista2/zad4/zad4.jl
@@ -1,13 +1,4 @@
-# Pkg.add("Polynomials")
-
 using Polynomials
-
-function printVector(V)
-    for i = 1:length(V)
-        println(V[i])
-    end
-    println()
-end
 
 p1 = [1, -210.0, 20615.0, -1256850.0,
     53327946.0, -1672280820.0, 40171771630.0, -756111184500.0,
@@ -28,17 +19,24 @@ p2 = [1, -210.0-(1.0/(2.0^23)), 20615.0, -1256850.0,
 n = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0,
     11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0, 18.0, 19.0, 20.0]
 
-function getPolyRoots(data)
-    P = Poly(data)
-    A = roots(P)
-    return A
-end
+#1
+#reverse order in vector
+p1 = p1[end:-1:1]
+
+#calculate roots
+calc_roots = roots(Poly(p1))
+
+#polyval from normal version of polynomial p
+# for i = 1:20
+    # println(abs(polyval(Poly(p1), roots[i])))
+# end
 
 #1
 # println("P(x) roots: ")
 # printVector(getPolyRoots(flipdim(p1, 1)))
-P = Poly(flipdim(p1, 1))
-A = roots(P)
+# P = Poly(flipdim(p1, 1))
+# printVector(A)
+# A = roots(P)
 
 #2
 # println("p(x) roots:")
@@ -48,7 +46,15 @@ A = roots(P)
 # println("Modified P(x) roots:")
 # printVector(getPolyRoots(flipdim(p2, 1)))
 
-println("Natural Wilkinson's:")
-for i = 1:20
-    println("P($i) = $(P(A[i]))")
+# println("Natural Wilkinson's:")
+# for i = 1:20
+#     println("P($i) = $(P(A[i]))")
+# end
+
+
+function printVector(V)
+    for i = 1:length(V)
+        println(V[i])
+    end
+    println()
 end
